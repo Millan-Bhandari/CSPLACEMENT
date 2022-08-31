b62c571e1276e0c80b622607ef552088689a3ac4
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-31 10:09:16 -0500

    commit

--- CSPLACEMENT/ContentView.swift
+++ CSPLACEMENT/ContentView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isodd = false
+    @State private var num1 = ""
+    @State private var text = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextField("Enter num", text: $num1)
+        Button("Submit") {
+            is_odd()
+            returnvar()
+        }
+        Text(text)
     }
+    func is_odd(){
+        if let num = Int(num1) {
+            if num % 2 == 0 {
+                isodd = false
+            }
+            else {
+                isodd = true
+            }
+        }
+    }
+    func returnvar() {
+        if isodd == true {
+            text = "true"
+        }
+        else {
+            text = "false"
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
