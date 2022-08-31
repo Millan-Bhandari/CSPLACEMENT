36e74b45c187b5ce5f5398b6430f99ec5eed45d2
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-31 08:46:43 -0500

    Initial Commit

---
+++ CSPLACEMENT/CSPLACEMENTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSPLACEMENTApp.swift
+//  CSPLACEMENT
+//
+//  Created by Millan Bhandari on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CSPLACEMENTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
