5b562dcbe24e9d1a790d719e7f715460aa62e42b
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-31 19:47:18 -0500

    commit

--- CSPLACEMENT/ContentView.swift
+++ CSPLACEMENT/ContentView.swift
@@ -1,34 +1,30 @@
-//
-//  ContentView.swift
-//  CSPLACEMENT
-//
-//  Created by Millan Bhandari on 8/31/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State var imput1 = ""
-    @State var imput2 = ""
-    @State var final = 0
+    @State var num1 = ""
+    @State var num2 = ""
+    @State var text = 0
     var body: some View {
         HStack {
             Text("Num 1")
-            TextField("", text: $imput1)
+            TextField("", text: $num1)
             Text("Num 2")
-            TextField("", text: $imput2)
+            TextField("", text: $num2)
         }
         Button("Submit") {
-            distance_between()
+            contains()
         }
-        Text("\(final)")
+        Text("\(text)")
     }
-    func distance_between() {
-        if let imputA = Int(imput1) {
-            if let imputB = Int(imput2) {
-                final = abs(imputA - imputB)
-            }
-        }
+    func contains() {
+        var numA = Int(num1)!
+        let numB = Int (num2)!
+        numA += 1
+        let range: CountableRange<Int> = numA..<numB
+        let array = Array(range)
+        let total = array.reduce(0, +)
+        text = total
+
     }
 }
 
